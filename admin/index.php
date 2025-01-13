01abc3993476595ce5d9b430427149da91dd4a6f
Brook Tekle (Opshadow) <126861167+Brooktek@users.noreply.github.com>
2025-01-13 14:10:01 +0300

    update
    updated some parts

--- admin/index.php
+++ admin/index.php
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Food Order Website</title>
-  <link rel="stylesheet" href="../css/admin.css">
-</head>
-<body>
-  <div class="menu text-center">
-    <div class="wrapper">
-      <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="manage-admin.php">Admin</a></li>
-        <li><a href="manage-category.php">Category</a></li>
-        <li><a href="manage-food.php">Food</a></li>
-        <li><a href="manage-order.php">Order</a></li>
-        <li><a href="logout.php">Logout</a></li>
-      </ul>
-    </div>
-  </div>  
+<?php
+include("partials/menu.php");
 
+?>
+ 
   <div class="main-content">
     <div class="wrapper">
       <h1>Dashboard</h1>
@@ -47,22 +30,13 @@
         Categories
       </div>    
       
-      <div class="col-4 text-center">
-        <h1>5</h1>
-        <br /> 
-        Categories
-      </div>
     <div class="clearfix"></div>
 
     </div>  
   </div>  
 
-  <div class="footer">
-    <div class="wrapper">
-      <p class="text-center">2020 All Rights reserved, some Restaurant. Develope by "OUR TEAM" </p>
-    </div>
-  </div>   
+  <?php
+  include("partials/footer.php");
+  ?>
 
-
-</body>
-</html>
+  
---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+    <div class="wrapper">
+      <p class="text-center">2020 All Rights reserved, some Restaurant. Develope by "OUR TEAM" </p>
+    </div>
+  </div>   
+
+
+</body>
+</html>
